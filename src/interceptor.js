00bdc9e56580a6685c8f708de3a7fce37403b128
deividcuello <deividcuellocuevas@gmail.com>
2024-03-15 21:28:10 -0400

    first commit

--- src/interceptor.js
+++ src/interceptor.js
@@ -2,20 +2,25 @@ import axios from "axios";
 let refresh = false;
 
 export default axios.interceptors.response.use(resp => resp, async error => {
-    if(error.response.status === 401 && !refresh){
-        refresh = true;
-        const response = await axios.post('https://deividcuello.pythonanywhere.com/api/auth/token/refresh/', {refresh:localStorage.getItem('refreshToken')}, {headers: {'Content-Type': 'application/json'}}, {withCredentials: true})
-        
-        if(response.status === 200) {
-            axios.defaults.headers.common['Authorization'] = `Bearer ${response.data['access']}`;
+    try{
+        if(error.response.status === 401 && !refresh){
+            refresh = true;
+            const response = await axios.post('https://deividcuello.pythonanywhere.com/api/auth/token/refresh/', {refresh:localStorage.getItem('refreshToken')}, {headers: {'Content-Type': 'application/json'}}, {withCredentials: true})
             
-            localStorage.setItem('accessToken', response.data.access);
-            localStorage.setItem('refreshToken', response.data.refresh);
-
-            return axios(error.config);
+            if(response.status === 200) {
+                axios.defaults.headers.common['Authorization'] = `Bearer ${response.data['access']}`;
+                
+                localStorage.setItem('accessToken', response.data.access);
+                localStorage.setItem('refreshToken', response.data.refresh);
+    
+                return axios(error.config);
+            }
+    
         }
-
+        refresh = false;
+        return error
     }
-    refresh = false;
-    return error
+    catch(error){
+        ''
+    }
 })
